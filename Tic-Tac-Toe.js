7565ca15b9946cbd8c363c3b1faf12e335ee79bb
Jordan Blin <jordanblin-83470@hotmail.com>
2024-06-25 20:40:01 +0200

    exo ia vs ia

--- Tic-Tac-Toe.js
+++ Tic-Tac-Toe.js
@@ -47,12 +47,18 @@ function addListenerToCells(){
             }
 
             grid[index] = currentPlayer;
-            cell.innerHTML = currentPlayer === 1 ? player1 : player2;
+            displayPlayerSymbol(index)
+            
 
             // On cherche si quelqu'un à gagné
-            checkIfSomeoneWon()           
+            checkIfSomeoneWon()
         });
     });
+}
+
+// Fonction qui permet d'afficher le symbole du joueur sur le DOM
+function displayPlayerSymbol(index){
+    document.querySelectorAll(".cell")[index].innerHTML = currentPlayer === 1 ? player1 : player2;
 }
 
 // On doit vérifier s'il y a un vainqueur
@@ -77,8 +83,7 @@ function checkIfSomeoneWon(){
         } else  {
             score.player2++
         }
-    }
-
+    } 
     // On vérifie si la partie est finie
     isGameFinished = grid.every(cell => cell !== 0);
 
@@ -96,9 +101,108 @@ function checkIfSomeoneWon(){
     initScore()
 }
 
+//=======================================================
+//fonction qui permet de de changer de joueur
+function changePlayer(){
+    currentPlayer = currentPlayer === 1 ? 2: 1;
+}
+
+function playWithAI(){
+    if (stopgameFlag) return;
+
+    let randomIndex = math.floor(math.random() * 9);
+    while (grid[randomIndex] !== 0) {
+        randomIndex = mathf,floor(math.random() * 9);
+    }
+}
+
+grid[randomIndex] = currentPlayer;
+displayPlayerSymbol(randomIndex);
+checkIfSomeoneWon();
+
+if(!isGameWon && !isGameFinished){
+    changePlayer();
+    setTimeout(playWithAI, math.random() * 750);
+} else if (isGameFinished){
+    setTimeout(newGame, 750)
+}
+
+function newGame(){
+    currentPlayer = 1;
+    isGameWon = false;
+    isGameFinished = false
+    grid = [0, 0, 0, 
+            0, 0, 0, 
+            0, 0, 0];
+
+    document.querySelectorAll('.cell').forEach(cell => {
+        cell.innerHTML = '';
+    });
+    setTimeout(playWithAI, 750);
+}
+
+function startGame(){
+    stopgameFlag = false;
+    newGame();
+}
+
+function stopgame() {
+    stopgameFlag = true;
+}
+
+
+//========================================================
 // Fonction qui pemet de changer le tour du joueur
 function changePlayer(){
     currentPlayer = currentPlayer === 1 ? 2 : 1;
+
+    // Si le joueur est le joueur 2, on doit lui permettre de jouer avec l'IA Debilla
+    if(currentPlayer === 2){
+        playWithDebilla()
+    }
+}
+//Fonction qui permet de jouer l'Ai vs IA
+function playWithMe(){
+
+    //On dois choisir un chiffre aléatoire entre 0 et 8
+    let randomIndex = Math.floor(Math.random() * 9);
+    console.log('randomIndex ', randomIndex)
+}
+
+// Fonction qui permet de jouer avec l'IA Debilla
+function playWithDebilla(){
+    // Faire le code pour que Debilla puisse jouer
+
+    /*
+        Pour que Debilla puisse jouer, elle doit pouvoir connaitre les cases vides
+        et choisir une case vide aléatoirement
+
+        Première version simple :
+        - Débilla choisit un chiffre entre 0 et 8 aléatoirement
+        - Si la case est vide, elle joue
+        - Sinon, elle recommence a choisir un chiffre aléatoirement
+
+        Une fois que l'ia à joué, on doit vérifier si elle a gagné
+        On doit relancer la fonction changePlayer pour que le joueur 1 puisse jouer  
+    */
+
+    // On choisi un nombre alétatoire entre 0 et 8
+    let randomIndex = Math.floor(Math.random() * 9);
+    console.log('randomIndex ', randomIndex)
+
+    // On vérifie si la case est vide
+    if(grid[randomIndex] === 0){
+        // La case est vide, on peut jouer
+        grid[randomIndex] = currentPlayer;
+
+        // On affiche le symbole du joueur sur le DOM
+        displayPlayerSymbol(randomIndex)
+        // On test pour savoir si quelqu'un a gagné
+        checkIfSomeoneWon()
+    } else {
+        // La case n'est pas vide, on recommence
+        playWithDebilla()
+    }
 }
 
 // Fonction qui permet de recommencer une partie
--- readme.md
+++ readme.md
@@ -1,40 +1,10 @@
-///IA joue 'O', joueur humain joue 'X'
-///Configuration actuelle :
- 
-c [ ][ ][ ]
-b [ ][X][ ]
-a [ ][ ][ ]
-   1  2  3
- 
-/// L'IA va tester tout les coups possibles pour 'O'
- 
-c [ ][ ][ ]
-b [ ][X][ ]
-a [O][ ][ ]
-   1  2  3
- 
-/// Et simuler l'impact de ce coup, c'est a dire simuler le prochain coup de l'adversaire.
- 
-c [ ][ ][ ]
-b [ ][X][ ]
-a [O][X][ ]
-   1  2  3
- 
-/// ici :
-/// Si l'IA joue a3 :
- 
-c [ ][ ][ ]
-b [ ][X][ ]
-a [O][X][O]
-   1  2  3
- 
-/// Elle va perdre, renvoi d'une valeur négative.
- 
-alors que si elle joue c2 :
- 
-c [ ][O][ ]
-b [ ][X][ ]
-a [O][X][O]
-   1  2  3
- 
-/// Elle renverra un poids nul (0) car personne ne gagne.
+AI versus IA
+
+-Faites en sorte que l'IA joue contre elle même.
+
+Ce qui est attendu :
+
+-Vous devez avoir un bouton qui permet de lancer les parties
+-L'ia doit relancer automatiquement une nouvelle partie
+-A chaque coup, l'ia doit "réfléchir" entre 0 et 750ms
+-Vous devez avoir un bouton qui permet de stopper les parties
